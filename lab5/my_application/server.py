ce6266b4490286b5090544a6cb02010b305f1666
AadamBari <aadambari@msn.com>
2015-10-14 18:36:16 +0300

    no

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -2,21 +2,21 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Index Page'
+    return "Index Page"
 
-@app.route('/hello')
+@app.route("/hello")
 def hello():
-    return 'Hello World'
+    return "Hello World"
 
-@app.route('/user/paul')
+@app.route("/user/paul")
 def projects():
     return 'User paul'
 
-@app.route('/post/80')
+@app.route("/post/80")
 def projects():
-    return 'Post 80'
+    return "Post 80"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',	port=8080,debug=True)

08d27a3a8cc2f4daf5d5de206dc5f5fc83b8e60c
AadamBari <aadambari@msn.com>
2015-10-14 15:01:21 +0000

    more edits to server.py

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -4,10 +4,15 @@ app = Flask(__name__)
 @app.route("/hello")
 def hello():
     return "Hello World!"
+  
+  @app.route("/user/paul")
+def projects():
+    return "user paul'"
+  
+  @app.route("/post/80")
+def projects():
+    return "Post 80"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',	port=8080,debug=True)
 
-@app.route('/projects/')
-def projects():
-    return 'The project page'
